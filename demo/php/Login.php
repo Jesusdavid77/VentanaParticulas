cb3db167727c9213d9e11bd2c2809a44836e514b
Jesusdavid77 <jesusdavidrinconrivera@hotmail.com>
2020-07-01 15:47:13 -0500

    //:~ Porfin se puede conectar a la base de datos me quiero matar
    Co-Authored-By: Monki77 <Jesusdavid77@users.noreply.github.com>

--- demo/php/Login.php
+++ demo/php/Login.php
@@ -1,25 +1,16 @@
 <?php
-  $conexion = mysqli_connect("localhost","root", "" )or die ("No se ha podido conectar al servidor de Base de datos");
-  $db = mysqli_select_db( $conexion, "resident_evil" ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
-
-
+//:~coneccion
+  $conexion = mysqli_connect("localhost","root", "" );
+//:~Revisa la coneccion y se une a la base de datos resident_evil
+  $db = mysqli_select_db( $conexion, "resident_evil" ) ;
+//:~Varialbe de la Contraseña
   $Contraseña=$_POST["Contraseña"];
-
-
-  $consulta = "INSERT INTO (contraseñas) VALUES ('aaa')";
+//:~Mete la variable Contraseña en la columna contraseña
+  $consulta = "INSERT INTO contraseñas VALUES ('$Contraseña')";
+//:~No se que hace pero es importante tu solo ponlo
   $resultado = mysqli_query( $conexion, $consulta );
-
-
-
-  if (mysql_query($conexion,$consulta)) {
-    echo "Insercion correcta de datos ";
-  }else {
-    echo "Error No se inserto el dato ";
-  }
-  mysqli_close($conexions);
-
-  mysqli_close( $conexion );
-
-
-echo $Contraseña;
+//:~Cierra la coneccion
+  mysqli_close($conexion);
+//:~Muestra la contraseña en pantalla
+  echo $Contraseña;
  ?>
